f3fecee30ddb99ea16ad6d59958a11fb3c8a2b5f
Daniel Esparza <daniel_esparza94@outlook.coma>
2021-02-12 12:32:04 -0600

    Validaciones
    nombre, correo, email, edad, user y URL

--- js/jsvalidator.js
+++ js/jsvalidator.js
@@ -7,20 +7,26 @@ class JSValidator {
 
     via = 'http';
 
-    msgs = {
+    validators = {
 
-        required: `Este campo es requerido`,
-        minLength: `Longitud no válida. Mínimo ${this.validators.minLength} caracteres`,
-        maxLength: `Longitud no válida. Máximo ${this.validators.maxLength} caracteres`,
-        email: `El campo de email no es válido`,
-        integer: `El campo debe ser de tipo entero`,
-        digit: `El valor debe ser un dígito`,
-        url: `El campo debe ser una URL válida`
+        minLength: 3,
+        maxLength: 255,
 
     }
 
+    msgs = {
 
-    constructor(formId) { //El cosntrusctor recibe como parametro el ID del formulario
+        required: `Este campo es requerido`,
+        minLength: `Longitud no válida. Mínimo __minLength__ caracteres`,
+        maxLength: `Longitud no válida. Máximo __maxLength__ caracteres`,
+        email: `El campo de email no es valido`,
+        integer: `Coloca un número entero`,
+        alphanumeric: `Solo se permiten letras y numeros sin espacios `,
+        url: `Escribe una URL valida (http:// o https//)`,
+    }
+
+
+    constructor(formId) { //El constructor recibe como parametro el ID del formulario
 
         //Definimos el formulario
         this.setForm(formId);
@@ -260,9 +266,109 @@ JSValidator.prototype._required = function (input) {
 
     }
 
-}
+};
 
 JSValidator.prototype._length = function (input) {
 
+    let value = input.value;
+
+    let inputLength = value.length;
+
+    let minLength = (input.dataset.validators_minLength !== undefined) ?
+        Number(input.dataset.validators_minlength) : this.validators.minLength;
+
+    let maxLength = (input.dataset.validators_maxLength !== undefined) ?
+        Number(input.dataset.validators_maxlength) : this.validators.maxLength;
+
+    let msg;
+
+    if (inputLength < minLength) {
+
+        msg = this.msgs.minLength.replace(`__minLength__`, minLength);
+
+        this.setError(input, msg);
+
+    }
+
+    if (inputLength > maxLength) {
+
+        msg = this.msgs.maxLength.replace(`__maxLength__`, maxLength);
+
+        this.setError(input, msg);
+
+    }
+
+};
+
+JSValidator.prototype._email = function (input) {
+
+    // Recuperar el valor de input 
+    let value = input.value;
+
+    // Definir el mensaje de error
+    let msg = this.msgs.email;
+
+    //Expresion regular para validar el email
+    let pattern = new RegExp(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i);
+
+    // Si la validació falla mandar el error
+    if (!pattern.test(value) && value.trim() != "") {
+
+        this.setError(input, msg);
+
+    }
+
+};
+
+JSValidator.prototype._integer = function (input) {
+
+    let value = input.value;
+
+    let msg = this.msgs.integer;
+
+    let pattern = new RegExp(/^[0-9]+$/);
+
+    if (!pattern.test(value) && value.trim() != "") {
+
+        this.setError(input, msg);
+
+    }
+
+};
+
+JSValidator.prototype._alphanumeric = function (input) {
+
+    let value = input.value;
+
+    let msg = this.msgs.alphanumeric;
+
+    let pattern = new RegExp(/^[a-zA-Z0-9]+$/);
+
+    if (!pattern.test(value) && value.trim() != "") {
+
+        this.setError(input, msg);
+
+    }
+
+};
+
+
+JSValidator.prototype._url = function (input) {
+
+    // En primer lugar vamos a recuperar el valor del input
+    let value = input.value;
+
+    // Definir el mensaje de error
+    let msg = this.msgs.url;
+
+    // expresión regular para validar url
+    var pattern = new RegExp(/^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i);
+
+    // En caso de que la validación falle mandar error.
+    if (!pattern.test(value) && value.trim() != "") {
+
+        this.setError(input, msg);
+
+    }
 
 }
